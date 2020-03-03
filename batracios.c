4fb573c51a114ba40195a79a0bfaa76f1959083d
GallegoProgrammer <56319483+GallegoProgrammer@users.noreply.github.com>
2020-03-03 16:26:11 +0100

    archivo Francisco

--- batracios.c
+++ batracios.c
@@ -1,49 +1,84 @@
-#include <stdio.h>
+#include "batracios.h"
 #include <stdlib.h>
+#include <stdio.h>
 #include <signal.h>
 #include <sys/sem.h>
-#include <sys/stat.h>
-#include <sys/msg.h>
-#include <sys/shm.h>
+#include <sys/stat.h> // tuberías
+#include <sys/msg.h> // buzones
+#include <sys/shm.h> // memoria compartida
 #include <sys/types.h>
 #include <unistd.h>
-#include "batracios.h"
+
 #define MAX_SEMAFOROS 32
-int semaforos[MAX_SEMAFOROS] = {-1}; //Quizás no sea necesario un array, mirar semget.
-int idMemoria = -1;
 
-void sigintHandler(int sig);
-void limpiarRecursos(void);
+/* ----------- PROTOTIPOS -------------- */
+	void sigintHandler(int sig);
+	void limpiarRecursos(void);
+/* ------------------------------------- */
 
-int main(int argc, char * argv) {
-	idMemoria = shmget(IPC_PRIVATE, 4096, IPC_CREAT | 0600);
-	if (idMemoria < 0) {
+//SEMÁFOROS
+int semaforos[MAX_SEMAFOROS] = {-1};
+// BUZÓN
+// MEMORIA COMPARTIDA
+int idMemoria;
+
+
+/* ------------------------------------ */
+	int main(int argc, char * argv) {
+/* ------------------------------------ */
+	struct sembuf sops[MAX_SEMAFOROS];
+	struct sigaction sigint;
+	sigint.sa_handler = sigintHandler;
+
+// PASO 1. CREAR SEMÁFORO Y MEMORIA COMPARTIDA
+	// SEMÁFORO
+	semaforos[0] = semget(IPC_PRIVATE,MAX_SEMAFOROS,IPC_CREAT | 0600);
+
+	// MEMORIA COMPARTIDA
+	idMemoria = shmget(IPC_PRIVATE,4096,IPC_CREAT | 0600);
+	if(idMemoria < 0){
 		perror("main: shmget");
 		return 1;
 	}
+	
+	// AQUÍ VA EL POSIBLE BUZÓN
+	//
+	//
+	//
+
+// PASO 2. CAPTAR CTRL+C Y ELIMINAR IPCS
+	if(sigaction(SIGINT,&sigint,NULL)==-1)
+		return 2;
+
 	while(1);
 	limpiarRecursos();
 	return 0;
 }
 
-void sigintHandler(int sig) {
+/* ----------------------------------- */
+	void sigintHandler(int sig){
+/* ----------------------------------- */
 	limpiarRecursos();
-	_exit(0);
+	_exit(0); // mejor para trabajar con señales
 }
 
-void limpiarRecursos(void) {
+/* ---------------------------- */
+	void limpiarRecursos(){
+/* ---------------------------- */
 	int i;
 	struct shmid_ds shmid;
-	for (i = 0; i < MAX_SEMAFOROS; i++) {
-		if (semaforos[i] > 0) {
-			if (semctl(semaforos[i], 0, IPC_RMID) < 0) {
+
+	for(i=0; i<MAX_SEMAFOROS; i++){
+		if(semaforos[i] > 0){
+			if(semctl(semaforos[i], 0, IPC_RMID) < 0){
 				perror("limpiarRecursos: semctl");
-				_exit(2);
+				_exit(3);
 			}
 		}
 	}
-	if (shmctl(idMemoria, IPC_RMID, &shmid) < 0) {
-		perror("limpiarMemoria: shmctl");
-		_exit(3);	
+
+	if(shmctl(idMemoria,IPC_RMID, &shmid) < 0){
+		perror("limpiarRecursos: shmctl");
+		_exit(4);
 	}
 }
