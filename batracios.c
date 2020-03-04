c3fc1552b7af3304e2846154051075307e467a0e
Josde <jorgecruz@usal.es>
2020-03-04 12:48:42 +0100

    Re-comentado y cambios de estilo ligeros

--- batracios.c
+++ batracios.c
@@ -9,11 +9,10 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define MAX_SEMAFOROS 25
-#define MIN_VELOCIDAD 0
-#define MAX_VELOCIDAD 1000
-#define MIN_TMEDIO 0
-
+#define MAX_SEMAFOROS (const int)25
+#define MIN_VELOCIDAD (const int)0
+#define MAX_VELOCIDAD (const int)1000
+#define MIN_TMEDIO (const int)0
 
 /* ----------- PROTOTIPOS -------------- */
 	void sigintHandler(int sig);
@@ -21,10 +20,10 @@
 /* ------------------------------------- */
 
 //SEMÁFOROS
-int semaforo;
+int semaforo = -1; // Iniciamos los IDs de los recursos IPC a -1, como se recomienda, para gestionar errores mejor
 // BUZÓN
 // MEMORIA COMPARTIDA
-int idMemoria;
+int idMemoria = -1;
 
 
 /* ------------------------------------ */
@@ -37,62 +36,71 @@ int idMemoria;
 	int vectorTroncos[7] = {3,4,5,6,7,8,9};
     int vectorAgua[7] = {7, 6, 5, 4, 3, 2, 1};
 	int vectorDirs[7] = {DERECHA, IZQUIERDA, DERECHA, IZQUIERDA, DERECHA, IZQUIERDA, DERECHA};
-	char * memoriaCompartida;
+	char * memoriaCompartida = NULL;
 // PASO 3. CONTROLAR PASO DE ARGUMENTOS
-	if(argc <=2){
-		printf("USO: ./batracios <velocidad> <tiempo medio>\n");
+	if(argc <= 2){
+		printf("USO: ./batracios <velocidad> <tiempo medio>.\n");
 		return 5;
 	}
 
 	arg1 = atoi(argv[1]);
 	arg2 = atoi(argv[2]);
 
-	if(arg1<MIN_VELOCIDAD || arg1>MAX_VELOCIDAD){
-		printf("<velocidad> debe estar entre %d y %d\n", MIN_VELOCIDAD, MAX_VELOCIDAD);
+	if(arg1 < MIN_VELOCIDAD || arg1 > MAX_VELOCIDAD) {
+		printf("<velocidad> debe estar entre %d y %d.\n", MIN_VELOCIDAD, MAX_VELOCIDAD);
 		return 6;
-	} else if(arg2<=MIN_TMEDIO){
-		printf("<tiempo medio> debe ser mayor que %d\n", MIN_TMEDIO);
+	} else if(arg2 <= MIN_TMEDIO) {
+		printf("<tiempo medio> debe ser mayor que %d.\n", MIN_TMEDIO);
 		return 7;
 	}
 
 // PASO 1. CREAR SEMÁFORO Y MEMORIA COMPARTIDA
 	// SEMÁFORO
 	semaforo = semget(IPC_PRIVATE, MAX_SEMAFOROS, IPC_CREAT | 0600);
-
+	if (semaforo < 0) {
+		perror("main: semget");
+		return 8;
+	}
 	// MEMORIA COMPARTIDA
 	idMemoria = shmget(IPC_PRIVATE,4096,IPC_CREAT | 0600);
-	if(idMemoria < 0){
+	if (idMemoria < 0) {
 		perror("main: shmget");
 		return 1;
 	}
 	memoriaCompartida = shmat(idMemoria, 0, 0); // el sistema decide dónde guardar la memoria compartida // no queremos flags especiales
-	// AQUÍ VA EL POSIBLE BUZÓN
-	//
-	//
-	//
+	if (memoriaCompartida == NULL) {
+		perror("main: shmat");
+		return 9;
+	}
+	/* 
+	/ AQUÍ VA EL POSIBLE BUZÓN
+	*/
 
 // PASO 2. CAPTAR CTRL+C Y ELIMINAR IPCS
-	if(sigaction(SIGINT,&sigint,NULL)==-1)
-		return 2;
+	if(sigaction(SIGINT,&sigint,NULL)==-1) return 2;
+		
+// PASO 4. Llamar a BATR_inicio y BATR_fin
 	BATR_inicio(arg1, semaforo, vectorTroncos, vectorAgua, vectorDirs, arg2, memoriaCompartida);
 	BATR_fin;
 	limpiarRecursos();
 	return 0;
 }
 
+
+
 /* ----------------------------------- */
-	void sigintHandler(int sig){
+	void sigintHandler(int sig) {
 /* ----------------------------------- */
 	limpiarRecursos();
 	_exit(0); // mejor para trabajar con señales
 }
 
 /* ---------------------------- */
-	void limpiarRecursos(){
+	void limpiarRecursos() 
 /* ---------------------------- */
 	int i;
 	struct shmid_ds shmid;
-    
+
 	if(semaforo > 0){
 		if(semctl(semaforo, 0, IPC_RMID) < 0){
 			perror("limpiarRecursos: semctl");
