706662c2c9eba81abfa7d26328feec622ebf78ab
Josde <jorgecruz@usal.es>
2020-03-06 13:15:12 +0100

    Hechos puntos 5, 6 y 7.

--- batracios.c
+++ batracios.c
@@ -13,10 +13,13 @@
 #define MIN_VELOCIDAD (const int)0
 #define MAX_VELOCIDAD (const int)1000
 #define MIN_TMEDIO (const int)0
+#define MOVIMIENTO_TRONCOS (const int)0
+
 
 /* ----------- PROTOTIPOS -------------- */
 	void sigintHandler(int sig);
 	void limpiarRecursos(void);
+	int bucleRanasMadre(int i);
 /* ------------------------------------- */
 
 //SEMÁFOROS
@@ -30,9 +33,10 @@ int idMemoria = -1;
 	int main(int argc, char * argv[]) {
 /* ------------------------------------ */
 	struct sembuf sops[MAX_SEMAFOROS];
-	struct sigaction sigint;
+	struct sigaction sigint, viejoSigint;
 	sigint.sa_handler = sigintHandler;
 	int arg1, arg2;
+	int i, retornoFork = 0;
 	int vectorTroncos[7] = {3,4,5,6,7,8,9};
     int vectorAgua[7] = {7, 6, 5, 4, 3, 2, 1};
 	int vectorDirs[7] = {DERECHA, IZQUIERDA, DERECHA, IZQUIERDA, DERECHA, IZQUIERDA, DERECHA};
@@ -77,11 +81,37 @@ int idMemoria = -1;
 	*/
 
 // PASO 2. CAPTAR CTRL+C Y ELIMINAR IPCS
-	if(sigaction(SIGINT,&sigint,NULL)==-1) return 2;
+	if(sigaction(SIGINT,&sigint,&viejoSigint)==-1) return 2;
 		
 // PASO 4. Llamar a BATR_inicio y BATR_fin
 	BATR_inicio(arg1, semaforo, vectorTroncos, vectorAgua, vectorDirs, arg2, memoriaCompartida);
-	BATR_fin;
+	// PASO 7. Crear procesos de las ranas madre
+	for (i = 0; i < 3; i++) {
+		switch(retornoFork) {
+			case 0:
+				retornoFork = fork();
+				if (retornoFork < 0) {
+					perror("main, creación ranas madre: fork");
+					return 10;
+				}
+				break;
+			default:
+				if(sigaction(SIGINT, &viejoSigint, NULL)==-1) return 11;
+				bucleRanasMadre(i);
+				break;
+		}
+ 	}
+	// PASO 5/6. Mover troncos
+	if (MOVIMIENTO_TRONCOS) {
+		while(1) {
+			for(i = 0; i < 7; i++) {
+				BATR_avance_troncos(i);
+				BATR_pausita();
+			}
+		}
+	}
+	while(1);
+	BATR_fin();
 	limpiarRecursos();
 	return 0;
 }
@@ -111,5 +141,13 @@ int idMemoria = -1;
 	if(shmctl(idMemoria,IPC_RMID, &shmid) < 0){
 		perror("limpiarRecursos: shmctl");
 		_exit(4);
+	}
+}
+
+int bucleRanasMadre(int i) {
+	int dx, dy;
+	while(1) {
+		BATR_descansar_criar();
+		BATR_parto_ranas(i, &dx, &dy);
 	}
 }
